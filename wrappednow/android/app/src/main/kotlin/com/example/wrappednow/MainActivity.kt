15f09c78ba2787de0c42ecdffa82ff5393ed04f6
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-01-13 22:36:20 +0000

    website base and app

---
+++ wrappednow/android/app/src/main/kotlin/com/example/wrappednow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wrappednow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wrappednow/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spotify Stats',
+      home: MyWebView(),
+    );
+  }
+}
+
+class MyWebView extends StatefulWidget {
+  @override
+  _MyWebViewState createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Spotify Stats'),
+      ),
+      body: WebView(
+        initialUrl: 'https://dragongr.github.io/spotify-stats/', // Replace with your web app URL
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
